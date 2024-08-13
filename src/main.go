c6408e0f515a4759c38cf676b1b8663f2c861c9c
Haoyu <kkkaslana@163.com>
2024-08-13 18:51:27 +0800

    替换glog为旧版本

--- src/main.go
+++ src/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"flag"
 	"signaling/src/framework"
+	"signaling/src/glog"
 )
 
 func main() {
+	flag.Parse()
+
 	err := framework.Init()
 
 	if err != nil {
 		panic(err)
 	}
+
+	glog.Info("Starting HTTP server")
+	glog.Flush()
 	err = framework.StartHttp()
 	if err != nil {
 		panic(err)
 
 	}
+
+
 }
