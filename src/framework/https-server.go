8370e5a776af2fd6f994bfafcf72be5f2e9642a6
Haoyu <kkkaslana@163.com>
2024-08-14 15:07:59 +0800

    完善ComLog

--- src/framework/https-server.go
+++ src/framework/https-server.go
@@ -3,6 +3,7 @@ package framework
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func init() {
@@ -13,6 +14,8 @@ func init() {
 type AcitonInterface interface {
 	Execute(w http.ResponseWriter, cr *ComRequest)
 }
+
+
 
 var GActionRouter map[string]AcitonInterface = make(map[string]AcitonInterface)
 
@@ -27,6 +30,16 @@ func responseError(w http.ResponseWriter, r *http.Request, status int, message s
 	w.Write([]byte(fmt.Sprintf("%d - %s", status, message)))
 }
 
+func getRealClientIP(r *http.Request) string {
+	clientIP := r.RemoteAddr
+	if realIP := r.Header.Get("X-Real-IP"); realIP != "" {
+		clientIP = realIP
+	} else if realIP := r.Header.Get("X-Forwarded-For"); realIP != "" {
+		clientIP = realIP
+	}
+	return clientIP
+}
+
 func entry(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/favicon.ico" {
 		w.WriteHeader(http.StatusOK)
@@ -37,8 +50,24 @@ func entry(w http.ResponseWriter, r *http.Request) {
 	if action, ok := GActionRouter[r.URL.Path]; ok {
 		if action != nil {
 			cr:=&ComRequest{R:r, Logger:&ComLog{}, LogId:GetLogId32()}
+			cr.Logger.AddNotice("logId",strconv.Itoa(int(cr.LogId)))
+			cr.Logger.AddNotice("url",r.URL.Path)
+			cr.Logger.AddNotice("referer",r.Header.Get("Referer"))
+			cr.Logger.AddNotice("cookie",r.Header.Get("cookie"))
+			cr.Logger.AddNotice("user-agent",r.Header.Get("User-Agent"))
+			cr.Logger.AddNotice("clientIP",r.RemoteAddr)
+			cr.Logger.AddNotice("realClientIP",getRealClientIP(r))
+
+
 			r.ParseForm()
+
+			for k,v := range r.Form{
+				cr.Logger.AddNotice(k,v[0])
+			}
+
+			cr.Logger.TimeBegin("totalCost")
 			action.Execute(w, cr)
+			cr.Logger.TimeEnd("totalCost")
 
 			cr.Logger.Infof("")
 		}else {
--- src/framework/log.go
+++ src/framework/log.go
@@ -3,6 +3,7 @@ import (
 	"signaling/src/glog"
 	"math/rand"
 	"time"
+	"fmt"
 
 )
 
@@ -13,18 +14,67 @@ func init(){
 func GetLogId32() uint32{
 	return rand.Uint32()
 }
-type ComLog struct {
+
+type logItem struct {
+	field string
+	value string
 }
 
 
+type timeItem struct {
+	field string
+	beginTime int64
+	endTime int64
+}
+type ComLog struct {
+	mainLog []logItem
+	timeLog []timeItem
+}
+
+func (l *ComLog) AddNotice(field string,value string){
+	l.mainLog = append(l.mainLog,logItem{field:field,value:value})
+}
+
+func (l *ComLog) TimeBegin(field string){
+	l.timeLog = append(l.timeLog,timeItem{field:field,beginTime:time.Now().UnixNano()/1000})
+}
+
+func (l *ComLog) TimeEnd(field string){
+	for k,v := range l.timeLog{
+		if v.field == field{
+			l.timeLog[k].endTime = time.Now().UnixNano()/1000
+			break
+		}
+	}
+
+}
+func (l *ComLog) GetPrefixLog() string{
+	prefixLog:=""
+	for _,item := range l.mainLog{
+		prefixLog += fmt.Sprintf("%s[%s]",item.field,item.value)
+	}
+
+	for _,timeitem := range l.timeLog{
+		diffTime:=timeitem.endTime-timeitem.beginTime
+		if diffTime<0{
+			continue
+		}
+		fdiffTime:=float64(diffTime)/1000.0
+		prefixLog += fmt.Sprintf("%s[%.3fms] ",timeitem.field,fdiffTime)
+	}
+	return prefixLog
+}
 func (c *ComLog) Debugf(format string,args ...interface{}) {
-	glog.Debugf(format,args...)
+	totalLog:=c.GetPrefixLog()+format
+	glog.Debugf(totalLog,args...)
 }
 
 func (c *ComLog) Infof(format string,args ...interface{}) {
-	glog.Infof(format,args...)
+	totalLog:=c.GetPrefixLog()+format
+	glog.Infof(totalLog,args...)
 }
 
 func (c *ComLog) Warningf(format string,args ...interface{}) {
-	glog.Warningf(format,args...)
+	totalLog:=c.GetPrefixLog()+format
+	glog.Warningf(totalLog,args...)
 }
